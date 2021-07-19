cc37be28b95fbb1edb1cb95821025b3fdfecda69
Valerie Tan <valerietan7@gmail.com>
2021-07-19 19:25:35 +0800

    added comments to code

--- p2p.js
+++ p2p.js
@@ -1,3 +1,7 @@
+// needs to find and connect peers, deploy servers that are used to discover other peers, and get an available TCP poty
+// discovery-swarm: used to create a network swarm that uses discovery-channel to find and connect peers
+// dat-swarm-defaults: deploys servers that used to discover other peers
+// get-port: gets available TCP ports
 const crypto = require("crypto");
 const Swarm = require("discovery-swarm");
 const defaults = require("dat-swarm-defaults");
@@ -16,7 +20,8 @@ const config = defaults({
 
 const swarm = Swarm(config);
 
-async () => {
+// async function to continuously monitor swarm.on event messages
+(async () => {
   const port = await getPort();
 
   swarm.listen(port);
@@ -28,6 +33,7 @@ async () => {
     const peerId = info.id.toString("hex");
     console.log(`Connected #${seq} to peer: ${peerId}`);
 
+    // ensure the network connection stays with peers
     if (info.initiator) {
       try {
         conn.setKeepAlive(true, 600);
@@ -48,6 +54,7 @@ async () => {
       console.log("----------- Received Message end -------------");
     });
 
+    // indicates you lost a connection with peers and delete from peers array
     conn.on("close", () => {
       console.log(`Connection ${seq} closed, peerId: ${peerId}`);
       if (peers[peerId].seq === seq) delete peers[peerId];
@@ -62,12 +69,13 @@ async () => {
     peers[peerId].seq = seq;
     connSeq++;
   });
-};
+})();
 
 setTimeout(() => {
   writeMessageToPeers("hello", null);
-});
+}, 10000);
 
+// send message to all connected peers
 writeMessageToPeers = (type, data) => {
   for (let id in peers) {
     console.log("-------- writeMessageToPeers start -------- ");
@@ -77,6 +85,7 @@ writeMessageToPeers = (type, data) => {
   }
 };
 
+// send message to specific peer via id
 writeMessageToPeerToId = (toId, type, data) => {
   for (let id in peers) {
     if (id === toId) {
@@ -88,6 +97,8 @@ writeMessageToPeerToId = (toId, type, data) => {
   }
 };
 
+// message needs to be a string and not an object
+// share message over p2p network
 sendMessage = (id, type, data) => {
   peers[id].conn.write(
     JSON.stringify({
