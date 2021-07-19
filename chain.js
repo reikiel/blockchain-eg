dacd652f86950883253e8cb6ce58dc30e8689840
Valerie Tan <valerietan7@gmail.com>
2021-07-19 19:49:22 +0800

    creating blockchain, genesis block and add get block functions

---
+++ chain.js
@@ -0,0 +1,41 @@
+let Block = require("./block").Block;
+let BlockHeader = require("./block").BlockHeader;
+let moment = require("moment");
+
+// returns genesis block
+let getGenesisBlock = () => {
+  let blockHeader = new BlockHeader(
+    1,
+    null,
+    "0x1bc3300000000000000000000000000000000000000000000",
+    moment().unix()
+  );
+  return new Block(blockHeader, 0, null);
+};
+
+let getLatestBlock = () => blockchain[blockchain.length - 1];
+
+let addBlock = (newBlock) => {
+  let prevBlock = getLatestBlock();
+  if (
+    prevBlock.index < newBlock.index &&
+    newBlock.blockHeader.previousBlockHeader ===
+      prevBlock.blockHeader.merkleRoot
+  ) {
+    blockchain.push(newBlock);
+  }
+};
+
+let getBlock = (index) => {
+  if (blockchain.length - 1 >= index) return blockchain[index];
+  else return null;
+};
+
+const blockchain = [getGenesisBlock()];
+
+if (typeof exports != "undefined") {
+  exports.addBlock = addBlock;
+  exports.getBlock = getBlock;
+  exports.blockchain = blockchain;
+  exports.getLatestBlock = getLatestBlock;
+}
