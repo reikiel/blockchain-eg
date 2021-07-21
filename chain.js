c5ed4689069b6ad38eed92ff39f31fc7ea890fda
Valerie Tan <valerietan7@gmail.com>
2021-07-21 13:51:59 +0800

    added generate new block function

--- chain.js
+++ chain.js
@@ -1,6 +1,7 @@
 let Block = require("./block").Block;
 let BlockHeader = require("./block").BlockHeader;
 let moment = require("moment");
+let CryptoJS = require("crypto-js");
 
 // returns genesis block
 let getGenesisBlock = () => {
@@ -33,9 +34,32 @@ let getBlock = (index) => {
 
 const blockchain = [getGenesisBlock()];
 
+const generateNextBlock = (txns) => {
+  const prevBlock = getLatestBlock();
+  const prevMerkleRoot = prevBlock.blockHeader.merkleRoot;
+  const nextIndex = prevBlock.index + 1;
+  const nextTime = moment().unix();
+  const nextMerkleRoot = CryptoJS.SHA256(
+    1,
+    prevMerkleRoot,
+    nextTime
+  ).toString();
+
+  const blockHeader = new BlockHeader(
+    1,
+    prevMerkleRoot,
+    nextMerkleRoot,
+    nextTime
+  );
+  const newBlock = new Block(blockHeader, nextIndex, txns);
+  blockchain.push(newBlock);
+  return newBlock;
+};
+
 if (typeof exports != "undefined") {
   exports.addBlock = addBlock;
   exports.getBlock = getBlock;
   exports.blockchain = blockchain;
   exports.getLatestBlock = getLatestBlock;
+  exports.generateNextBlock = generateNextBlock;
 }
